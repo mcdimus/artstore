0aa3edb4ba9821474735775dfc0e03a69aa8000e
Dmitri Maksimov <dmitri.maksimov@nortal.com>
2020-01-16 16:07:16 +0200

    Added Spring Boot rest service.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.jetbrains.kotlin.jvm") version "1.3.41"
+  kotlin("jvm") version "1.3.61"
+  kotlin("plugin.spring") version "1.3.61"
   id("org.springframework.boot") version "2.2.2.RELEASE"
   id("io.spring.dependency-management") version "1.0.8.RELEASE"
+  // uncomment to build a deployable artifact
+  //war
 }
+
+version="1.0.0-SNAPSHOT"
 
 repositories {
   jcenter()
@@ -12,7 +17,9 @@ repositories {
 
 dependencies {
   implementation(kotlin("stdlib-jdk8"))
+  implementation(kotlin("reflect"))
   implementation("org.springframework.boot:spring-boot-starter")
+  implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
 tasks.withType<KotlinCompile> {
@@ -21,6 +28,11 @@ tasks.withType<KotlinCompile> {
     jvmTarget = "11"
   }
 }
+
+// uncomment to build a deployable artifact
+//tasks.war {
+//  isEnabled = true
+//}
 
 tasks.wrapper {
   gradleVersion = "6.1"
